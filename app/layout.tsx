1ad2f206dd50c4afca0304c37ba7375c8d36ebbd
Ankan Mukherjee <ankanmukherjee011@gmail.com>
2025-05-08 13:15:29 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'FarmSense',
+  title: 'F',
   description: 'Created by Ankan',
   generator: 'ankanz1',
 }
